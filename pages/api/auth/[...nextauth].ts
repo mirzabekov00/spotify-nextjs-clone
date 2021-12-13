1d68f8fdc17a84e6f722f4d936dc88434edd38be
mirzabekov00 <prog.eldar@gmail.com>
2021-12-13 23:15:33 +0200

    feat(login): initialize spotify auth provider using next-auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,54 @@
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
+import get from "lodash.get";
+import type { JWT } from "next-auth/jwt";
+
+const refreshAccessToken = async (token: JWT) => {
+  try {
+    // spotifyApi.setAccessToken(token.)
+    return {};
+  } catch (error) {
+    console.error(error);
+
+    return { ...token, error: "RefreshAccessTokenError" };
+  }
+};
+
+export default NextAuth({
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      authorization: LOGIN_URL,
+    }),
+  ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      const accessToken = get(account, "access_token", null);
+      const refreshToken = get(account, "refresh_token", null);
+      const username = get(account, "providerAccountId", null);
+      const accessTokenExpires = get(account, "expires_at", 0);
+
+      if (account && user) {
+        return {
+          ...token,
+          accessToken,
+          refreshToken,
+          username,
+          accessTokenExpires: accessTokenExpires * 1000,
+        };
+      }
+
+      if (Date.now() < accessTokenExpires) {
+        return token;
+      }
+
+      return refreshAccessToken(token);
+    },
+  },
+});
---
+++ pages/login.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Login: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Login | Spotify Clone</title>
+        <link rel="/favicon.ico" />
+      </Head>
+
+      <div>login</div>
+    </>
+  );
+};
+
+export default Login;
