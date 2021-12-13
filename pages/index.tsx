c6bd573cd720b9b79c541a86ccd4d7517209190d
mirzabekov00 <prog.eldar@gmail.com>
2021-12-13 22:19:20 +0200

    feat(sidebar): add sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import Sidebar from "../components/Sidebar";
 
 const Home: NextPage = () => {
-  return <h1 className="text-indigo-600">Hello world!</h1>;
+  return (
+    <>
+      <Head>
+        <title>Spotify Clone</title>
+        <link rel="/favicon.ico" />
+      </Head>
+
+      <main className="bg-black h-screen overflow-hidden">
+        <Sidebar />
+      </main>
+
+      <div></div>
+    </>
+  );
 };
 
 export default Home;
