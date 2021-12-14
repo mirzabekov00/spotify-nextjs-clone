0f488076b9156a2df15bde080a54177843995908
mirzabekov00 <prog.eldar@gmail.com>
2021-12-14 23:06:00 +0200

    feat(login): add refresh token functional

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,12 +2,21 @@ import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
 import get from "lodash.get";
-import type { JWT } from "next-auth/jwt";
+import { JWT } from "next-auth/jwt";
 
 const refreshAccessToken = async (token: JWT) => {
   try {
-    // spotifyApi.setAccessToken(token.)
-    return {};
+    spotifyApi.setAccessToken(token.accessToken || "");
+    spotifyApi.setRefreshToken(token.refreshToken || "");
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000,
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    };
   } catch (error) {
     console.error(error);
 
@@ -28,7 +37,7 @@ export default NextAuth({
     signIn: "/login",
   },
   callbacks: {
-    async jwt({ token, account, user }) {
+    jwt: async ({ token, account, user }): Promise<JWT> => {
       const accessToken = get(account, "access_token", null);
       const refreshToken = get(account, "refresh_token", null);
       const username = get(account, "providerAccountId", null);
@@ -49,6 +58,13 @@ export default NextAuth({
       }
 
       return refreshAccessToken(token);
+    },
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+
+      return session;
     },
   },
 });
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,21 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
+import {
+  ClientSafeProvider,
+  getProviders,
+  LiteralUnion,
+  signIn,
+} from "next-auth/react";
+import { BuiltInProviderType } from "next-auth/providers";
 
-const Login: NextPage = () => {
+interface LoginProps {
+  providers: Record<
+    LiteralUnion<BuiltInProviderType, string>,
+    ClientSafeProvider
+  > | null;
+}
+
+const Login: NextPage<LoginProps> = ({ providers }) => {
   return (
     <>
       <Head>
@@ -9,9 +23,32 @@ const Login: NextPage = () => {
         <link rel="/favicon.ico" />
       </Head>
 
-      <div>login</div>
+      <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+        <img className="w-52 mb-5" src="https://i.imgur.com/fPuEa9V.png" />
+        {providers &&
+          Object.values(providers).map((provider) => (
+            <div key={provider.id}>
+              <button
+                className="bg-[#18D860] text-white px-5 py-3 rounded-lg"
+                onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+              >
+                Login with {provider.name}
+              </button>
+            </div>
+          ))}
+      </div>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<LoginProps> = async () => {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
 };
 
 export default Login;
